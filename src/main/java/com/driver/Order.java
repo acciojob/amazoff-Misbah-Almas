9df3c7c0392ad7cb3747cfbcd6c0de058e80ca8e
Misbah Almas <dev.misbahalmas@gmail.com>
2023-02-25 21:33:04 +0530

    Attempt1

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,5 +1,11 @@
 package com.driver;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
 
     private String id;
@@ -9,11 +15,26 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        String arr[] = deliveryTime.split(":");
+        int hours = Integer.parseInt(arr[0]);
+        int min = Integer.parseInt(arr[1]);
+        int timeOfDelivery = hours*60 + min;
+
+        this.id = id;
+        this.deliveryTime = timeOfDelivery;
     }
 
     public String getId() {
         return id;
     }
 
+    public void setDeliveryTime(String deliveryTime){
+        String arr[] = deliveryTime.split(":");
+        int hours = Integer.parseInt(arr[0]);
+        int min = Integer.parseInt(arr[1]);
+        int timeOfDelivery = hours*60 + min;
+
+        this.deliveryTime = timeOfDelivery;
+    }
     public int getDeliveryTime() {return deliveryTime;}
 }
